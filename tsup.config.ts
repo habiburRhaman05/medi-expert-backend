6155f19dd8fdac25167c5c5ef29faf8ed3d56995
habiburRhaman05 <devhabib2005@gmail.com>
2026-04-05 22:07:23 +0600

    cleanuped tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "tsup";
-import { execSync } from "child_process";
+import fs from "fs";
 import path from "path";
-import  fs from "fs";
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
@@ -27,13 +26,11 @@ export default defineConfig({
     "fs",
     "url"
   ],
-  // 🔑 Build shesh hole templates folder-ti dist-e copy korbe
+
   async onSuccess() {
     const srcDir = path.join(__dirname, "src/templates");
     const destDir = path.join(__dirname, "dist/templates");
-
     if (fs.existsSync(srcDir)) {
-      // Node.js 16.7+ version-e recursive copy support kore
       fs.cpSync(srcDir, destDir, { recursive: true });
       console.log("✅ Templates successfully copied to dist/templates");
     } else {
